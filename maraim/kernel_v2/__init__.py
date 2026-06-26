48928cd905e16f6202e028f9146f5c49d127f8d6
generatorhost <generatorhost@gmail.com>
2026-06-26 20:49:59 +0300

    Export modular event bus and memory engines

--- maraim/kernel_v2/__init__.py
+++ maraim/kernel_v2/__init__.py
@@ -1,6 +1,8 @@
 from .runtime_object import RuntimeObject, RuntimeState, RuntimeIdentity, DNARuntime
 from .runtime_graph import RuntimeGraph
 from .dna_manager import DNAManager
+from .event_bus import EventBusEngine
+from .memory_engine import MemoryEngine
 from .engines import KernelV2
 from .lifecycle import RuntimeLifecycleManager
 from .resource_manager import RuntimeResourceManager
