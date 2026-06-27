21c650e087855dd53c69ce50dbe3eb671909c56a
generatorhost <generatorhost@gmail.com>
2026-06-27 14:43:27 +0300

    Include permission sandbox in preflight

--- scripts/kernel_v2_preflight.py
+++ scripts/kernel_v2_preflight.py
@@ -24,6 +24,7 @@ MODULES = [
     "maraim.kernel_v2.task_graph_v2",
     "maraim.kernel_v2.execution_adapter_v2",
     "maraim.kernel_v2.result_artifact_v2",
+    "maraim.kernel_v2.permission_sandbox",
     "maraim.kernel_v2",
 ]
 
