ea20a3d9faf3b97f318e6b244d0d8ba78bd35988
generatorhost <generatorhost@gmail.com>
2026-06-28 12:41:46 +0300

    Export production bridge phase3 package

--- maraim/kernel_v2/production_bridge/__init__.py
+++ maraim/kernel_v2/production_bridge/__init__.py
@@ -1,4 +1,11 @@
 from .steps import ProductionBridgeSteps
 from .phase2 import ProductionBridgePhase2, PHASE2_STEPS
+from .phase3 import ProductionBridgePhase3, PHASE3_STEPS
 
-__all__ = ["ProductionBridgeSteps", "ProductionBridgePhase2", "PHASE2_STEPS"]
+__all__ = [
+    "ProductionBridgeSteps",
+    "ProductionBridgePhase2",
+    "ProductionBridgePhase3",
+    "PHASE2_STEPS",
+    "PHASE3_STEPS",
+]
