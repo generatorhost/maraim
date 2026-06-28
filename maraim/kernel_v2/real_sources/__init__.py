693d5350530cb6b09a666ea141a0c1d5134db796
generatorhost <generatorhost@gmail.com>
2026-06-28 08:39:29 +0300

    Add real sources package exports

---
+++ maraim/kernel_v2/real_sources/__init__.py
@@ -0,0 +1,9 @@
+from .contracts import RealSourceContract
+from .plans import RealSourceExecutionPlan
+from .readiness import RealSourceReadiness
+
+__all__ = [
+    "RealSourceContract",
+    "RealSourceExecutionPlan",
+    "RealSourceReadiness",
+]
