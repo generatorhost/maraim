e5c4a93b5e064ef121edfec0413c4653bd147434
generatorhost <generatorhost@gmail.com>
2026-06-28 19:27:03 +0300

    Export runtime workspace manager

--- maraim/kernel_v2/__init__.py
+++ maraim/kernel_v2/__init__.py
@@ -40,6 +40,7 @@ from .real_engines_roadmap import RealEnginesRoadmap, REAL_ENGINE_STAGES
 from .git_adapter_plan import GitAdapterImplementationPlan, GIT_ADAPTER_PLAN_STEPS
 from .archive_adapter_plan import ArchiveAdapterImplementationPlan, ARCHIVE_ADAPTER_PLAN_STEPS
 from .folder_adapter_plan import FolderAdapterImplementationPlan, FOLDER_ADAPTER_PLAN_STEPS
+from .runtime_workspace_manager import RuntimeWorkspaceManager
 from .phase3_foundation import (
     AdapterContractRegistry,
     SandboxContractRegistry,
